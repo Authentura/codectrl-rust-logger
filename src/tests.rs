51607c8cce800a84c21c441d874c2579d81539dd
Samuel Boyden <sam@stboyden.com>
2022-07-03 18:06:33 +0100

    Port connection method to gRPC

--- src/tests.rs
+++ src/tests.rs
@@ -8,7 +8,7 @@ fn log() { log_layer_2(); }
 fn log_if() { log_if_layer_2() }
 
 #[test]
-fn log_if_env() { log_if_env_layer_2() }
+fn log_when_env() { log_when_env_layer_2() }
 
 // normal log
 fn log_layer_2() { log_layer_3(); }
@@ -16,7 +16,7 @@ fn log_layer_2() { log_layer_3(); }
 fn log_layer_3() { log_final_layer(); }
 
 fn log_final_layer() {
-    let a = || crate::Log::log("Hello", Some(2), None, None);
+    let a = || crate::Logger::log("Hello", Some(2), None, None, None);
     let _ = a();
 }
 
@@ -30,24 +30,32 @@ fn log_if_layer_3() { log_if_final_layer() }
 fn log_if_final_layer() {
     let some_variable = true;
 
+    let _ = crate::Logger::log_if(
+        log_if_gen_random,
+        "Hello, conditional",
+        Some(2),
+        None,
+        None,
+        None,
+    );
     let _ =
-        crate::Log::log_if(log_if_gen_random, "Hello, conditional", Some(2), None, None);
-    let _ = crate::Log::log_if(|| true, "Hello, conditional 2", Some(2), None, None);
+        crate::Logger::log_if(|| true, "Hello, conditional 2", Some(2), None, None, None);
 
-    let _ = crate::Log::boxed_log_if(
+    let _ = crate::Logger::boxed_log_if(
         Box::new(move || some_variable),
         "Hello, conditional 3",
         Some(2),
         None,
         None,
+        None,
     );
 }
 
-// log_if_env
-fn log_if_env_layer_2() { log_if_env_layer_3() }
+// log_when_env
+fn log_when_env_layer_2() { log_when_env_layer_3() }
 
-fn log_if_env_layer_3() { log_if_env_final_layer() }
+fn log_when_env_layer_3() { log_when_env_final_layer() }
 
-fn log_if_env_final_layer() {
-    let _ = crate::Log::log_if_env("Hello, world env", Some(2), None, None);
+fn log_when_env_final_layer() {
+    let _ = crate::Logger::log_when_env("Hello, world env", Some(2), None, None, None);
 }
